78a35b6f4a70cc2d0ec1c34a1980f4b0ffe89fe1
Andrey <andrey.kappix.haines@gmail.com>
2020-04-15 16:50:24 +0300

    added auth header for handling JWT

--- src/app/MCBLabberAPI/mcblabber-api.service.ts
+++ src/app/MCBLabberAPI/mcblabber-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { JWT } from '../auth/jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,18 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
 export abstract class MCBLabberAPIService {
 
   protected URL: string = 'https://localhost:2772';
-  protected headers: HttpHeaders;
+  protected headers: HttpHeaders = new HttpHeaders();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    let jwt: JWT = JSON.parse(localStorage.getItem('JWT'));
+    this.headers.set('Authorization', jwt?.access_token);
+   }
 
   protected get(additURL: string){
-      return this.http.get(this.URL + additURL, {headers: this.headers});
+    return this.http.get(this.URL + additURL, {headers: this.headers});
   }
 
   protected post(additURL: string, body: any){
-    return this.http.post(this.URL + additURL, body);
+    return this.http.post(this.URL + additURL, body, {headers: this.headers});
   }
 }
